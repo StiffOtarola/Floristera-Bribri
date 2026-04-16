10e43726a723c0c3a8e4e9052de4ba2b55cd5e1c
Berny Otarola <bernycrack123456789@gmail.com>
2026-04-16 11:55:53 -0600

    fix: productos destacados en home ahora abren detalle del producto

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -619,18 +619,22 @@
         <div class="prod-grid">
             @foreach($productos as $p)
                 <div class="prod-card">
-                    <div class="prod-img">
-                        @if($p->imagen)
-                            <img src="{{ asset('storage/products/' . $p->imagen) }}" alt="{{ $p->nombre }}">
-                        @else
-                            <div class="prod-ph">💐</div>
-                        @endif
-                        <div class="prod-badge">Destacado</div>
-                    </div>
-                    <div class="prod-body">
-                        <div class="prod-cat">{{ $p->categoria->nombre ?? 'Flores' }}</div>
-                        <div class="prod-name">{{ $p->nombre }}</div>
-                        <div class="prod-desc">{{ $p->descripcion }}</div>
+                    <a href="{{ route('catalogo.show', $p->id) }}" style="text-decoration:none;color:inherit;">
+                        <div class="prod-img">
+                            @if($p->imagen)
+                                <img src="{{ asset('storage/products/' . $p->imagen) }}" alt="{{ $p->nombre }}">
+                            @else
+                                <div class="prod-ph">💐</div>
+                            @endif
+                            <div class="prod-badge">Destacado</div>
+                        </div>
+                        <div class="prod-body">
+                            <div class="prod-cat">{{ $p->categoria->nombre ?? 'Flores' }}</div>
+                            <div class="prod-name">{{ $p->nombre }}</div>
+                            <div class="prod-desc">{{ $p->descripcion }}</div>
+                        </div>
+                    </a>
+                    <div class="prod-body" style="padding-top:0;">
                         <div class="prod-footer">
                             <div class="prod-price">{{ formatPrice($p->precio) }}</div>
                             <button class="btn-agregar"
